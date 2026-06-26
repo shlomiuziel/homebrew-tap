c5e4e570b31ba16cde1eb50350442b357064e1b2
Shlomi Uziel <shlomi.uziel@shlomis-macbook-pro-3.local>
2026-06-26 21:47:42 +0300

    Install prebuilt macOS helper

--- Formula/sonosh.rb
+++ Formula/sonosh.rb
@@ -4,18 +4,29 @@ class Sonosh < Formula
   url "https://github.com/shlomiuziel/sonosh/archive/refs/tags/v0.3.2-sonosh.1.tar.gz"
   sha256 "3bd7c05d3f599bbc9f7f2d36eed418acfe7db2ade1b853a4833109991ade55c3"
   version "0.3.2-sonosh.1"
-  head "https://github.com/shlomiuziel/sonosh.git", branch: "main"
+  license "MIT"
 
   depends_on "go" => :build
+
+  resource "sonosh-helper-darwin-arm64" do
+    url "https://github.com/shlomiuziel/sonosh/releases/download/v0.3.2-sonosh.1/sonosh-helper-darwin-arm64.tar.gz"
+    sha256 :no_check
+  end
+
+  resource "sonosh-helper-darwin-amd64" do
+    url "https://github.com/shlomiuziel/sonosh/releases/download/v0.3.2-sonosh.1/sonosh-helper-darwin-amd64.tar.gz"
+    sha256 :no_check
+  end
 
   def install
     sonosh_bin = libexec/"sonosh"
     system "go", "build", "-o", sonosh_bin, "./cmd/sonosh"
 
     if OS.mac?
-      helper_path = buildpath/"helpers/macos/sonosh-helper"
-      system "swift", "build", "--package-path", helper_path, "--configuration", "release", "--disable-sandbox"
-      libexec.install helper_path/".build/release/sonosh-macos-helper"
+      helper_resource = Hardware::CPU.arm? ? "sonosh-helper-darwin-arm64" : "sonosh-helper-darwin-amd64"
+      resource(helper_resource).stage do
+        libexec.install "sonosh-macos-helper"
+      end
     end
 
     (bin/"sonosh").write <<~EOS
